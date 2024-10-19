80c3e2476d3520c655108d7c0467a0ad05cdcd90
JulianRivers <juliancamilorifo@ufps.edu.co>
2024-10-19 16:33:13 -0500

    chore: create base structure of the application

---
+++ app/src/main/java/com/drimo_app/views/HomeView.kt
@@ -0,0 +1,62 @@
+package com.drimo_app.views
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavController
+import com.drimo_app.components.Space
+
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun HomeView(navController: NavController){
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+//                    TitleBar(name = "Home view")
+                },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = Color.Red
+                )
+            )
+        },
+        floatingActionButton = {
+//            ActionButton()
+        }
+    ) {
+        ContentHomeView(navController)
+    }
+}
+
+@Composable
+fun ContentHomeView(navController: NavController){
+    val id = 123
+    var opcional by remember { mutableStateOf("") }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Space()
+        TextField(value = opcional, onValueChange = {opcional=it}, label = { Text(text = "Opcional") })
+//        MainButton(name = "Detail view", backColor = Color.Red, color = Color.White, onClick = {
+//            navController.navigate("Detail/${id}/?${opcional}")
+//        } )
+    }
+}
