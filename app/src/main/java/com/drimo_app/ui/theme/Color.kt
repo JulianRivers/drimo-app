5c912b54ea4df52ae2bcd13af844cfd7e5237970
JulianRivers <juliancamilorifo@ufps.edu.co>
2024-10-19 16:36:40 -0500

    chore: create app color palette

--- app/src/main/java/com/drimo_app/ui/theme/Color.kt
+++ app/src/main/java/com/drimo_app/ui/theme/Color.kt
@@ -2,10 +2,9 @@ package com.drimo_app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val LightBlue = Color(0xFF132A65)
+val MidnightBlue = Color(0xFF1F265E)
+val LightPurple = Color(0xFF8E97FD)
+val DarkBlue = Color(0xFF03174C)
+val White = Color(0xFFE8EAED)
+val LightGrey = Color(0xFFA1A4B2)
--- app/src/main/java/com/drimo_app/ui/theme/Theme.kt
+++ app/src/main/java/com/drimo_app/ui/theme/Theme.kt
@@ -12,15 +12,25 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = LightPurple,
+    secondary = MidnightBlue,
+    background = DarkBlue,
+    surface = DarkBlue,
+    onPrimary = White,
+    onSecondary = White,
+    onBackground = White,
+    onSurface = White
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = LightPurple,
+    secondary = MidnightBlue,
+    background = White,
+    surface = LightGrey,
+    onPrimary = White,
+    onSecondary = DarkBlue,
+    onBackground = DarkBlue,
+    onSurface = DarkBlue
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -41,11 +51,6 @@ fun Drimo_appTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
